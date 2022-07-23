d40a8876baa42cdf164d91b85d3b7a5318cf4fe2
Adilson Barreto <sejaassociado@gmail.com>
2022-07-23 11:09:23 -0300

    FEAT: Se aprofundando em funções e criando uma calculadora com JavaScript.

---
+++ tiposDeFuncao.js
@@ -0,0 +1,31 @@
+// tipos de função:
+
+// declarativas:
+/* function funcao() {
+    console.log("Essa mensagem veio de uma função declarativa!");
+}
+
+funcao(); */
+
+// expressão de função:
+// com declaração 
+/* var funcao = function() {
+    console.log("Sou uma mensagem de função de expressão!");
+}
+
+funcao();*/
+
+// arrow function:
+/* var funcao = () => {
+    console.log('Sou uma arrow function!');
+}
+
+funcao();*/
+
+
+
+
+
+
+
+
